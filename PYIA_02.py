31d63b1cbc1c91b7f34871547305f01cc7422270
LeonardoElias182 <leonardoelias182@gmail.com>
2024-11-24 19:41:27 -0300

    Add files via upload

---
+++ PYIA_02.py
@@ -0,0 +1,10 @@
+dicionario = {
+
+    'nome': input ('Digite o nome para contato: '),
+    'telefone': input ('Digite um telefone para contato: '),
+    'email': input ('Digite um email para contato: ')
+}
+
+print (dicionario)
+
+
